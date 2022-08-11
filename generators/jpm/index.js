e8d93f66151b6ad939d16240ca8f7795b4e5a8f0
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2022-08-11 15:14:14 +0200

    move jpm init to install

--- generators/jpm/index.js
+++ generators/jpm/index.js
@@ -1,8 +1,12 @@
 const Generator = require('yeoman-generator')
-const shell = require('shelljs')
+// const shell = require('shelljs')
 
 module.exports = class extends Generator {
   writing () {
-    shell.exec('npx @jolie/jpm init')
+    // shell.exec('npx @jolie/jpm init')
+  }
+
+  install () {
+    this.spawnCommand('npx', ['@jolie/jpm', 'init'])
   }
 }
